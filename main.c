79498e1fbbaf551e12ea4cb7112cac73985869f0
Allard <lyon.allard.chris@gmail.com>
2020-04-25 17:30:03 +0200

    type pions

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
 #include <iomanip>
 
+typedef struct Pions {
+    int type;
+    int posX;
+    int posY;
+} pion;
+
 int main(int argc, char *argv[]){
+
+    /* boucle principale */
+    while (1){
+
+    }
 
     return 0;
 }
